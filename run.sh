df6f482e3f876c9f757ccba53851c832493a2a10
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2020-06-09 05:50:03 +0300

    version change

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 export DISPLAY=:0
-java -jar console-math-app/target/console-math-app-3.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar console-math-app/target/console-math-app-5.0-SNAPSHOT-jar-with-dependencies.jar
